639d4cb9a2670e6bedcf9996f8c77d965ab7b2dd
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-07-13 18:12:46 +0200

    adding "make payment" mocked data

--- service/PaymentsControllerService.js
+++ service/PaymentsControllerService.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 /**
  * checkPayment
@@ -10,32 +9,31 @@
 exports.checkPaymentUsingGET = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "data" : {
-    "idPayment" : "idPayment",
-    "urlRedirectEc" : "urlRedirectEc",
-    "amount" : {
-      "amount" : 0,
-      "currencyNumber" : "currencyNumber",
-      "currency" : "currency",
-      "decimalDigits" : 6
-    },
-    "receiver" : "receiver",
-    "subject" : "subject",
-    "cancelled" : true,
-    "id" : 1,
-    "bolloDigitale" : true,
-    "email" : "email"
-  }
-};
+    examples["application/json"] = {
+      data: {
+        idPayment: "idPayment",
+        urlRedirectEc: "urlRedirectEc",
+        amount: {
+          amount: 0,
+          currencyNumber: "currencyNumber",
+          currency: "currency",
+          decimalDigits: 6
+        },
+        receiver: "receiver",
+        subject: "subject",
+        cancelled: true,
+        id: 1,
+        bolloDigitale: true,
+        email: "email"
+      }
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  * deletePayment
@@ -47,8 +45,7 @@ exports.deletePaymentUsingDELETE = function(id) {
   return new Promise(function(resolve, reject) {
     resolve();
   });
-}
-
+};
 
 /**
  * getPayment
@@ -59,32 +56,31 @@ exports.deletePaymentUsingDELETE = function(id) {
 exports.getPaymentUsingGET = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "data" : {
-    "idPayment" : "idPayment",
-    "urlRedirectEc" : "urlRedirectEc",
-    "amount" : {
-      "amount" : 0,
-      "currencyNumber" : "currencyNumber",
-      "currency" : "currency",
-      "decimalDigits" : 6
-    },
-    "receiver" : "receiver",
-    "subject" : "subject",
-    "cancelled" : true,
-    "id" : 1,
-    "bolloDigitale" : true,
-    "email" : "email"
-  }
-};
+    examples["application/json"] = {
+      data: {
+        idPayment: "idPayment",
+        urlRedirectEc: "urlRedirectEc",
+        amount: {
+          amount: 0,
+          currencyNumber: "currencyNumber",
+          currency: "currency",
+          decimalDigits: 6
+        },
+        receiver: "receiver",
+        subject: "subject",
+        cancelled: true,
+        id: 1,
+        bolloDigitale: true,
+        email: "email"
+      }
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
 
 /**
  * pay
@@ -93,53 +89,53 @@ exports.getPaymentUsingGET = function(id) {
  * payRequest PayRequest payRequest
  * returns TransactionResponse
  **/
-exports.payUsingPOST = function(id,payRequest) {
+exports.payUsingPOST = function(id, payRequest) {
+  const now = new Date().toISOString();
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "data" : {
-    "urlRedirectPSP" : "urlRedirectPSP",
-    "amount" : {
-      "amount" : 0,
-      "currencyNumber" : "currencyNumber",
-      "currency" : "currency",
-      "decimalDigits" : 6
-    },
-    "paymentModel" : 2,
-    "idPsp" : 1,
-    "grandTotal" : {
-      "amount" : 0,
-      "currencyNumber" : "currencyNumber",
-      "currency" : "currency",
-      "decimalDigits" : 6
-    },
-    "created" : "2000-01-23T04:56:07.000+00:00",
-    "fee" : {
-      "amount" : 0,
-      "currencyNumber" : "currencyNumber",
-      "currency" : "currency",
-      "decimalDigits" : 6
-    },
-    "description" : "description",
-    "merchant" : "merchant",
-    "error" : true,
-    "statusMessage" : "statusMessage",
-    "token" : "token",
-    "idPayment" : 6,
-    "idStatus" : 5,
-    "nodoIdPayment" : "nodoIdPayment",
-    "success" : true,
-    "urlCheckout3ds" : "urlCheckout3ds",
-    "id" : 0,
-    "idWallet" : 5,
-    "updated" : "2000-01-23T04:56:07.000+00:00"
-  }
-};
+    examples["application/json"] = {
+      data: {
+        urlRedirectPSP: "urlRedirectPSP",
+        amount: {
+          amount: 10052,
+          currencyNumber: "1",
+          currency: "EUR",
+          decimalDigits: 2
+        },
+        paymentModel: 2,
+        idPsp: 1,
+        grandTotal: {
+          amount: 10102,
+          currencyNumber: "1",
+          currency: "EUR",
+          decimalDigits: 2
+        },
+        created: now,
+        fee: {
+          amount: 50,
+          currencyNumber: "currencyNumber",
+          currency: "currency",
+          decimalDigits: 2
+        },
+        description: "IMU 2018",
+        merchant: "Comune di Canicattì",
+        error: false,
+        statusMessage: "all is well",
+        token: "token",
+        idPayment: 123,
+        idStatus: 5,
+        nodoIdPayment: "nodoIdPayment",
+        success: true,
+        urlCheckout3ds: "urlCheckout3ds",
+        id: Math.floor(Math.random() * 1000),
+        idWallet: payRequest.idWallet,
+        updated: now
+      }
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
       resolve();
     }
   });
-}
-
+};
